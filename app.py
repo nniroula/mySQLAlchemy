1028c3edb5996b8458e731286726df6da1652de0
nabin <nkbhaie2017@gmail.com>
2021-11-19 18:33:43 -0700

    add more config to sqlAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension 
 from flask_sqlalchemy import SQLAlchemy
+from models import db, connect_db  # db is instance db = SQLAlchemy()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'Postgresql:///movies' # movies is a database name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///movies_example' # movies is a database name
 
-db = SQLAlchemy()
-db.app = app # To associate our app with SQLAlchemy
-db.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress the warning message you get when you run app.py file
+
+app.config['SQLALCHEMY_ECHO'] = True  # this shows all sql commands being executed in ipython terminal when you run db.session.execute('select * from movies') like command
+
+# STEP 2
+# db = SQLAlchemy()
+# db.app = app # To associate our app with SQLAlchemy
+# db.init_app(app)
+
+app.config['SECRET_KEY'] = "nkbhaiebro"
+debug = DebugToolbarExtension(app)
+
+connect_db(app)   # STEP 2
+
+@app.route('/')
+def home():
+    """ shows home page"""
+    return render_template("home.html")
+    # return "Hello, sqlAlchemy experts"
